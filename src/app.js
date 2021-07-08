d8387dc0422060292b20e653d3d42d18ef1b592f
AidanMegabyte <aidan.megabyte@gmail.com>
2021-07-09 00:23:49 +0300

    Поправил верстку

--- src/app.js
+++ src/app.js
@@ -1,24 +1,34 @@
-const rssUrlId = 'rssUrl';
+const formId = 'rssForm';
+const rssUrlId = 'rssUrlInput';
+const rssAddBtnId = 'rssAddButton';
 
 const html = `
-<div class="container">
-  <div class="row">
-    <div class="col col-lg-2"></div>
+<main class="container-fluid flex-grow-1">
+  <section class="row bg-dark p-5">
+    <div class="col-2"></div>
     <div class="col">
-      <input type="text" class="form-control" id="${rssUrlId}" aria-describedby="rssUrlHelp">
+      <h1 class="display-4 pb-3 text-white">RSS-агрегатор</h1>
+      <form id="${formId}" class="text-body">
+        <div class="row">
+          <div class="col">
+            <div class="form-floating">
+              <input type="text" class="form-control" id="${rssUrlId}" placeholder="Ссылка RSS">
+              <label for="${rssUrlId}">Ссылка RSS</label>
+            </div>
+          </div>
+          <button id="${rssAddBtnId}" type="submit" class="col-auto btn btn-primary btn-lg px-sm-5">Добавить</button>
+        </div>
+      </form>
     </div>
-    <div class="col-md-auto">
-      <button type="submit" class="btn btn-primary">Добавить</button>
-    </div>
-    <div class="col col-lg-2"></div>
-  </div>
-</div>
+    <div class="col-2"></div>
+  </section>
+</main>
 `;
 
 const render = () => {
-  const form = document.createElement('form');
-  form.innerHTML = html;
-  document.body.appendChild(form);
+  document.body.classList.add('d-flex', 'flex-column', 'vh-100');
+  document.body.innerHTML = html;
+  document.querySelector(`#${rssUrlId}`).focus();
 };
 
 export default () => {
