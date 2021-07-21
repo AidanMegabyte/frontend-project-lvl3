f5179ce9819a31c4174379fe74f9aabff90e9104
AidanMegabyte <aidan.megabyte@gmail.com>
2021-07-21 13:29:38 +0300

    Fix v17.0

--- src/api.js
+++ src/api.js
@@ -1,5 +1,5 @@
 import axios from 'axios';
 
 export default {
-  getRssContent: (rssUrl) => axios.get(`https://hexlet-allorigins.herokuapp.com/get?url=${rssUrl}&disableCache=true`),
+  getRssContent: (rssUrl) => axios.get(`https://hexlet-allorigins.herokuapp.com/get?url=${encodeURIComponent(rssUrl)}&disableCache=true`),
 };
